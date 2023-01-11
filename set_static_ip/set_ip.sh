67b083148cf0550485e880b6654638c129537e66
appzicpvt <117719820+appzicpvt@users.noreply.github.com>
2023-01-11 12:09:51 +0530

    Update set_ip.sh

--- set_static_ip/set_ip.sh
+++ set_static_ip/set_ip.sh
@@ -5,10 +5,10 @@ echo "IPv4 address is 192.168.8.___"
 read -p 'Host: ' HOST
 
 if [ $HOST -gt 1 ] && [ $HOST -lt 255 ]; then
-    sed -i "s/HOST_NUM/$HOST/g" 00-installer-config.yaml && # change config file with host
-    sudo mv ./00-installer-config.yaml /etc/netplan/ && # move config file to /etc/netplan/
-    sudo netplan apply && # apply netplan
-    echo "IP Set Successfully!!!" &&
+    sed -i "s/HOST_NUM/$HOST/g" 00-installer-config.yaml && \ # change config file with host
+    sudo mv ./00-installer-config.yaml /etc/netplan/ && \ # move config file to /etc/netplan/
+    sudo netplan apply && \ # apply netplan
+    echo "IP Set Successfully!!!" && \
     ip address # show current ip address
 else
     echo "You enter wrong host number"
